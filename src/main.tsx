0dee3cd84a242e55643ebdba34159b7bbb650d8b
daryl800 <ng.daryl@gmail.com>
2025-08-26 20:49:07 +0800

    updated main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,18 @@
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+// import { createRoot } from 'react-dom/client'
+// import App from './App.tsx'
+// import './index.css'
 
-createRoot(document.getElementById("root")!).render(<App />);
+// createRoot(document.getElementById("root")!).render(<App />);
+
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import App from "./App";
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <BrowserRouter basename="/project-revealer/">
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
+);
