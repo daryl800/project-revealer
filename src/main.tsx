36f219c25257aef2f8fbfca30d6b5dd1645ace2a
daryl800 <ng.daryl@gmail.com>
2025-08-26 21:50:07 +0800

    added the missing homepage url value

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,5 @@
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
-import { BrowserRouter } from "react-router-dom";
 
-const root = createRoot(document.getElementById("root")!);
-
-root.render(
-  <BrowserRouter basename="/project-revealer/">
-    <App />
-  </BrowserRouter>
-);
+createRoot(document.getElementById("root")!).render(<App />);
